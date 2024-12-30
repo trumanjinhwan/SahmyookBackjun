9206a6943198aeff5ef61db82f1ba343f2068628
82104 <82104@DESKTOP-563FBS7>
2024-12-30 14:41:27 +0900

    sc 객체 닫기

--- Backjun/src/q2741/Main.java
+++ Backjun/src/q2741/Main.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int num = scanner.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
 		
 		for (int i=1; i <= num; i++) {
 			System.out.println(i);
 		}
+		sc.close();
 
 	}
 
