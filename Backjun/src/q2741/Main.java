04fdc8ecb419c4986a048ab699f4840e416e5c9d
82104 <82104@DESKTOP-563FBS7>
2024-12-30 14:06:24 +0900

    첫번째 커밋

---
+++ Backjun/src/q2741/Main.java
@@ -0,0 +1,17 @@
+package q2741;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		
+		for (int i=1; i <= num; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
